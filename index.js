eb389778f94dad582978f94d0a24a6b5214c3cf7
ajwbeard <71530556+ajwbeard@users.noreply.github.com>
2022-09-16 10:34:00 -0700

    Added jQuery, generalized expand/collapse functions, added postmastr section

--- index.js
+++ index.js
@@ -1,12 +1,54 @@
-function expand() {
-  document.getElementById('resume').innerHTML = `
-    <button class="expand-collapse"><img src="Images/collapse.png" onclick="collapse()"/></button>
-    <img class="resume" src="Files/resume.png"/>
+var collapseButton1 = "<button id='collapse-btn1' class='expand-collapse'><img src='Images/collapse.png' /></button>";
+var expandButton1 = "<button id='expand-btn1' class='expand-collapse'><img src='Images/expand.png' /></button>";
+const resume = "<img class='resume' src='Files/resume.png'/>";
+
+var collapseButton2 = "<button id='collapse-btn2' class='expand-collapse'><img src='Images/collapse.png' /></button>";
+var expandButton2 = "<button id='expand-btn2' class='expand-collapse'><img src='Images/expand.png' /></button>";
+const postmastr = `
+  <img class='resume' src='Images/postmastr-title.png' />
+  <img class='resume' src='Images/postmastr-intro.png' />
+  <img class='resume' src='Images/postmastr-solution.png' />
+  <img class='resume' src='Images/postmastr-frontend.png' />
+  <h2>Screens I made:</h2>
+  <img class='resume' src='Images/postmastr-login-signup.png' />
+  <img class='resume' src='Images/postmastr-scanned-packages.png' />
+  <img class='resume' src='Images/postmastr-scanned-packages3.png' />
+  <img class='resume' src='Images/postmastr-scanned-packages1.png' />
+  <img class='resume' src='Images/postmastr-scanned-packages2.png' />
   `;
+
+window.onload = function() {
+  document.getElementById('resume').innerHTML = collapseButton1;
+  document.getElementById('resume').innerHTML += resume;
+  $("#collapse-btn1").click(function() {
+    collapse('resume', "expand-btn1", 'collapse-btn1', expandButton1, collapseButton1, resume);
+  });
+
+  document.getElementById('postmastr').innerHTML = collapseButton2;
+  document.getElementById('postmastr').innerHTML += postmastr;
+  $("#collapse-btn2").click(function() {
+    collapse('postmastr', "expand-btn2", 'collapse-btn2', expandButton2, collapseButton2, postmastr);
+  });
+};
+
+//elementId is a string, the name of the element that is going to be expanded
+//contents is the HTML to be added into the element
+//btn is the collapse button
+//btnId is the ID of the collapse button located in the element
+function expand(elementId, btnExpId, btnCollId, btnExp, btnColl, contents) {
+  document.getElementById(elementId).innerHTML = btnColl;
+  document.getElementById(elementId).innerHTML += contents;
+  $("#" + btnCollId).click(function() {
+    collapse(elementId, btnExpId, btnCollId, btnExp, btnColl, contents);
+  });
 }
 
-function collapse() {
-  document.getElementById('resume').innerHTML = `
-    <button class="expand-collapse"><img src="Images/expand.png" onclick="expand()"/></button>
-  `;
+//elementId is a string, name of element that is going to be collapsed
+//btn is the expand button
+//btnId is the ID of the expand button located in the element
+function collapse(elementId, btnExpId, btnCollId, btnExp, btnColl, contents) {
+  document.getElementById(elementId).innerHTML = btnExp;
+  $("#" + btnExpId).click(function() {
+    expand(elementId, btnExpId, btnCollId, btnExp, btnColl, contents);
+  });
 }
